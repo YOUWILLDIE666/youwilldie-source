12b4a4e2cc2d8e440ed41f15480b6d1112b36038
YOU WILL DIE <i44538887@gmail.com>
2024-06-19 18:08:59 +0300

    random titles, broke somewhy (utf-8)

---
+++ uwu fuck me.js
@@ -0,0 +1,3 @@
+const titles = ["???", "？？？", "¿?¿", "⸮⸮⸮", "?̷̛̼͖̦̜̩̳̅̈́̒̕?̵̙̮̦̺͇͓̘͙̞͒̑̀͠?̴̢̺̹͖͖̟̔̏̂̓ͅͅ", "¿¿¿"];
+document.title = titles[Math.floor(Math.random() * titles.length)];
+//console.log(document.title);
