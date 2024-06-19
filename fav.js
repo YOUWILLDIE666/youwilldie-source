dd764053b713f4de34a5e6ceb60778df481d3428
YOU WILL DIE <i44538887@gmail.com>
2024-06-20 00:14:28 +0300

    random icon support
    Add an optional extended description...

---
+++ fav.js
@@ -0,0 +1,12 @@
+const t = ["tuhmb.png", "haskell2.png"];
+
+function i() {
+  const randomIndex = Math.floor(Math.random() * t.length);
+  const randomImage = t[randomIndex];
+  return `./${randomImage}`;
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  const linkElement = document.querySelector('link[rel="icon"]');
+  linkElement.href = i();
+});
