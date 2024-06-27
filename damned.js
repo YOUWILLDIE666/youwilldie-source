b2f87c6cc667b5fe5c7731a97b197bb5ca064b28
YOU WILL DIE <i44538887@gmail.com>
2024-06-27 16:26:20 +0300

    void text

--- damned.js
+++ damned.js
@@ -1,20 +1,10 @@
-const PI = Math.PI;
-
 function loop(e) {
     setTimeout(() => {
-        var y = Math.floor(Math.random() / PI * 25) + "px " + Math.floor(Math.random() ** PI + 5) / 1.5 + "px " + `#${Math.floor(Math.random() * 0x900000).toString(16).padStart(6, '0')}`
+        let y = Math.floor(Math.random() / Math.PI * 25) + "px " + Math.floor(Math.random() ** Math.PI + 5) / 1.5 + "px " + `#${Math.floor(Math.random() * 0x900000).toString(16).padStart(6, '0')}`
         e.style.textShadow = y.toString();
         e.style.opacity = Math.floor(Math.random() * 1000) + "%";
         loop(e);
     }, 35);
 }
 
-document.querySelectorAll("#void").forEach((e) => {
-    loop(e);
-});
-
-/*
-// in case if id....args.....
-document.querySelectorAll(".void").forEach((e) => {
-    loop(e);
-});*/
+document.querySelectorAll("#void").forEach((e) => { loop(e); });
