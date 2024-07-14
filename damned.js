bcc5d80d3265975e90cd2e6da134147bda586c76
YOU WILL DIE <i44538887@gmail.com>
2024-07-14 17:56:15 +0300

    void text

--- damned.js
+++ damned.js
@@ -1,6 +1,6 @@
 function loop(e) {
     setTimeout(() => {
-        let y = Math.floor(Math.random() / Math.PI * 25) + "px " + Math.floor(Math.random() ** Math.PI + 5) / 1.5 + "px " + `#${Math.floor(Math.random() * 0x900000).toString(16).padStart(6, '0')}`
+        let y = `${Math.floor(Math.random() / Math.PI * 25)}px ${Math.floor(Math.random() ** Math.PI + 5) / 1.5}px #${Math.floor(Math.random() * 0x900000).toString(16).padStart(6, '0')}`
         e.style.textShadow = y.toString();
         e.style.opacity = Math.floor(Math.random() * 1000) + "%";
         loop(e);
