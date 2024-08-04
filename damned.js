2941675f0959b6595f3c7cfb3a638c00c676d183
YOU WILL DIE <youwilldie6.066@gmail.com>
2024-08-04 20:28:15 +0300

    rewrite

--- damned.js
+++ damned.js
@@ -1,8 +1,8 @@
 function loop(e) {
     requestAnimationFrame(() => {
-        const randomHex = Math.floor(Math.random() * 0x900000).toString(16).padStart(6, '0');
-        const textShadow = `${Math.floor(Math.random() / Math.PI * 25)}px ${Math.floor(Math.random() ** Math.PI + 5) / 1.5}px #${randomHex}`;
-        e.style.textShadow = textShadow;
+        const rHex = Math.floor(Math.random() * 0x900000).toString(16).padStart(6, '0');
+        const Shadow = `${Math.floor(Math.random() / Math.PI * 25)}px ${Math.floor(Math.random() ** Math.PI + 5) / 1.5}px #${rHex}`;
+        e.style.textShadow = Shadow;
         e.style.opacity = `${Math.floor(Math.random() * 100)}%`; // changed to 100 instead of 1000
         loop(e);
     });
