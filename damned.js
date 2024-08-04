878a4b1d55649aef73ce96abb6cda25735bf20b0
Rest Athin <i44538887@gmail.com>
2024-08-04 20:23:47 +0300

    you don't need the :toString();

--- damned.js
+++ damned.js
@@ -1,7 +1,7 @@
 function loop(e) {
     setTimeout(() => {
         let y = `${Math.floor(Math.random() / Math.PI * 25)}px ${Math.floor(Math.random() ** Math.PI + 5) / 1.5}px #${Math.floor(Math.random() * 0x900000).toString(16).padStart(6, '0')}`
-        e.style.textShadow = y.toString();
+        e.style.textShadow = y;
         e.style.opacity = Math.floor(Math.random() * 1000) + "%";
         loop(e);
     }, 35);
